09538f5a28782f9cd4978444f48eb3d4305a8717
lukexuanliu <lukexuanliu@gmail.com>
2013-02-22 03:40:32 +0000

    not working! stuck at customize our header slide

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Info2310::Application.routes.draw do
 
   resources :micro_posts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy] #login
 
   get "static_pages/home"
   get "static_pages/help"
